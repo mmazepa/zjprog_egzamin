c5348b6448431f285e43640dac5a509a3e13ef4b
mmazepa <maksymilianmariusz@gmail.com>
2019-01-07 21:15:09 +0100

    Rozwinięcie metryk CKJM.

--- scripts/ckjm.sh
+++ scripts/ckjm.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+POINT=$1/src/Point.java
+if [ -f $POINT ]; then
+	java -jar tools/ckjm-1.9.jar $1/class/Point.class
+fi
+
 java -jar tools/ckjm-1.9.jar $1/class/EuclideanDistance.class
